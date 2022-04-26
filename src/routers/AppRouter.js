224b54b2c8b1a5dc7844089bc573443772b35b26
Elis Antonio Perez <elisperezmusic@gmail.com>
2022-04-26 16:53:43 -0500

    Private Route

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -2,13 +2,20 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { LoginScreen } from "../components/login/LoginScreen";
 import { DashboardRoutes } from "./DashboardRoutes";
+import { PrivateRoute } from "./PrivateRoute";
 
 export const AppRouter = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={ <LoginScreen /> } />
-        <Route path="/*" element={ <DashboardRoutes /> } />
+        <Route path="/*" element={
+          <PrivateRoute>
+            <DashboardRoutes />
+          </PrivateRoute>
+        }
+        />
+        {/* <Route path="/*" element={ <DashboardRoutes /> } /> */}
       </Routes>
     </BrowserRouter>
   );
