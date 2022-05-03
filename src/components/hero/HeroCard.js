b1a30d66983c8071b371f7b1d4d773e01745d3c1
Elis Antonio Perez <elisperezmusic@gmail.com>
2022-05-02 21:27:10 -0500

    heroImages helper

--- src/components/hero/HeroCard.js
+++ src/components/hero/HeroCard.js
@@ -1,4 +1,7 @@
 import { Link } from "react-router-dom";
+import { heroImages } from "../../helpers/heroImages";
+
+
 
 export const HeroCard = ({
  id,
@@ -9,14 +12,14 @@ export const HeroCard = ({
  characters
 }) => {
 
- const imagePath = `/assets/${id}.jpg`;
+//  const imagePath = `/assets/${id}.jpg`;
 
   return (
    <div className="col animate__animated animate__fadeIn">
     <div className="card bg-dark">
      <div className="row no-gutters">
       <div className="col-4">
-       <img src={ imagePath } alt={ superhero } className="card-img" />
+       <img src={ heroImages(`./${id}.jpg`) } alt={ superhero } className="card-img" />
       </div>
       <div className="col-8">
        <div className="card-body">
--- src/components/hero/HeroScreen.js
+++ src/components/hero/HeroScreen.js
@@ -1,59 +1,58 @@
-import { useMemo } from "react";
-import { Navigate, useNavigate, useParams } from "react-router-dom"
-import { getHeroById } from "../../selectors/getHeroById";
+import { useMemo } from 'react';
+import { Navigate, useNavigate, useParams } from 'react-router-dom';
+import { heroImages } from '../../helpers/heroImages';
+import { getHeroById } from '../../selectors/getHeroById';
 
 export const HeroScreen = () => {
+	const { heroeId } = useParams();
+	const navigate = useNavigate();
 
-  const { heroeId } = useParams();
-  const navigate = useNavigate();
+	const hero = useMemo(() => getHeroById(heroeId), [heroeId]);
 
-  const hero = useMemo(() => getHeroById(heroeId), [heroeId]);
+	const handleReturn = () => {
+		navigate(-1);
+	};
 
-  const handleReturn = () => {
-    navigate(-1);
-  }
+	if (!hero) {
+		return <Navigate to='/' />;
+	}
 
-  if (!hero) {
-    return <Navigate to='/' />
-  }
+	const { id, alter_ego, characters, first_appearance, publisher, superhero } =
+		hero;
 
-  
-  const { 
-    id,
-    alter_ego,
-    characters,
-    first_appearance,
-    publisher,
-    superhero
-  } = hero;
-  
-  const imagePath = `/assets/${id}.jpg`;
+	// const imagePath = `/assets/${id}.jpg`;
 
-  return (
-    <div className="row mt-5">
-      
-        <div className="col-4">
-          <img
-            src={ imagePath }
-            alt={ superhero }
-            className="img-thumbnail animate__animated animate__fadeInLeft"
-            />
-        </div>
-        <div className="col-8 animate__animated animate__fadeIn">
-          <h3>{superhero}</h3>
-          <ul className="list-group">
-            <li className="list-group-item bg-dark text-light"><b>Alter ego: </b>{ alter_ego }</li>
-            <li className="list-group-item bg-dark text-light"><b>Publisher: </b>{ publisher }</li>
-            <li className="list-group-item bg-dark text-light"><b>First Appearance: </b>{ first_appearance }</li>
-          </ul>
-          <h5 className="mt-3">Characters</h5>
-          <p>{ characters }</p>
-          <button
-            className="btn btn-outline-light"
-            onClick={ handleReturn }
-          >Return</button>
-        </div>
-      
-   </div>
-  )
-}
+	return (
+		<div className='row mt-5'>
+			<div className='col-4'>
+				<img
+					src={heroImages(`./${id}.jpg`)}
+					alt={superhero}
+					className='img-thumbnail animate__animated animate__fadeInLeft'
+				/>
+			</div>
+			<div className='col-8 animate__animated animate__fadeIn'>
+				<h3>{superhero}</h3>
+				<ul className='list-group'>
+					<li className='list-group-item bg-dark text-light'>
+						<b>Alter ego: </b>
+						{alter_ego}
+					</li>
+					<li className='list-group-item bg-dark text-light'>
+						<b>Publisher: </b>
+						{publisher}
+					</li>
+					<li className='list-group-item bg-dark text-light'>
+						<b>First Appearance: </b>
+						{first_appearance}
+					</li>
+				</ul>
+				<h5 className='mt-3'>Characters</h5>
+				<p>{characters}</p>
+				<button className='btn btn-outline-light' onClick={handleReturn}>
+					Return
+				</button>
+			</div>
+		</div>
+	);
+};
